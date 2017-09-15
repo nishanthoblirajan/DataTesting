1cd5604111ef88d24ff49414ca9c38eac97eeeb4
nishanthoblirajan <obli.oblirajan.13@ucl.ac.uk>
2017-09-15 14:43:42 +0530

    listview

--- app/src/main/java/com/zaptrapp/lynda1/MainActivity.java
+++ app/src/main/java/com/zaptrapp/lynda1/MainActivity.java
@@ -2,8 +2,11 @@ package com.zaptrapp.lynda1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -11,15 +14,18 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvOut;
+    ListView mListView;
 
     List<DataItem> mDataItemList = SampleDataProvider.sDataItemList;
 
+    List<String> itemNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvOut = (TextView) findViewById(R.id.put);
+        mListView = (ListView) findViewById(R.id.listView);
 
         //clear the textView
         tvOut.setText("");
@@ -34,9 +40,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Display the data in the textView
-        for(DataItem item:mDataItemList){
-            tvOut.append(item.getItemName()+"\n");
+        for (DataItem item : mDataItemList) {
+//            tvOut.append(item.getItemName()+"\n");
+            itemNames.add(item.getItemName());
         }
+        Collections.sort(itemNames);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemNames);
 
+        mListView.setAdapter(adapter);
     }
 }
